358c0413438a03becf03d8e1313c015b28167b52
Josh <joshuasevendavid@gmail.com>
2022-11-27 15:02:08 -0500

    Prezi URL added

--- ConstructorLiveAssignment.java
+++ ConstructorLiveAssignment.java
@@ -1,25 +1,25 @@
-package com.java.constructordemo;
-import java.util.Scanner;
-public class ConstructorLiveAssignment {
-    //Note that both files are seen here but ARE separate files, as they are in separate classes
-    //First class: ConstructorLiveAssignemtn
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Get simple user input and display it
-        System.out.println("Prompt user for input");
-        //create Object(s) of the class below
-        mandatoryOptional manOpt = // complete your object; feel free to change its name!
-
-
-    }
-}
-    //Second file/class: mandatoryOptional
-    //Not that the files are separated in the Project structure on the left
-class mandatoryOptional {
-    // create two mandatory fields and one optional field minimum
-
-    // create Constructor(s) to handle input of mandatory and optional information
-
-    // you may create getter and setter methods to take in and output user info
-
-}
+//package com.java.constructordemo;
+//import java.util.Scanner;
+//public class ConstructorLiveAssignment {
+//    //Note that both files are seen here but ARE separate files, as they are in separate classes
+//    //First class: ConstructorLiveAssignemtn
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        // Get simple user input and display it
+//        System.out.println("Prompt user for input");
+//        //create Object(s) of the class below
+//        mandatoryOptional manOpt = // complete your object; feel free to change its name!
+//
+//
+//    }
+//}
+//    //Second file/class: mandatoryOptional
+//    //Not that the files are separated in the Project structure on the left
+//class mandatoryOptional {
+//    // create two mandatory fields and one optional field minimum
+//
+//    // create Constructor(s) to handle input of mandatory and optional information
+//
+//    // you may create getter and setter methods to take in and output user info
+//
+//}
