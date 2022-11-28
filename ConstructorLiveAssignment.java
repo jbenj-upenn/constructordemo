9aa22b5f68849078188e3bc73bb40486fdd73097
Josh <joshuasevendavid@gmail.com>
2022-11-28 13:00:26 -0500

    solution to live assignment: middle name is optional; calling one constructor from another

--- ConstructorLiveAssignment.java
+++ ConstructorLiveAssignment.java
@@ -5,21 +5,58 @@ public class ConstructorLiveAssignment {
     //First class: ConstructorLiveAssignemtn
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int again = 0;
+        String fName, mName, lName;
+        int ssn;
+        int userNumber = 0;
+
         // Get simple user input and display it
         System.out.println("Prompt user for input");
         //create Object(s) of the class below
-        mandatoryOptional manOpt = new mandatoryOptional(); // complete your object; feel free to change its name!
+        do {
+
+            System.out.println("Enter first name: ");
+            fName = sc.nextLine();
+            System.out.println("Enter middle name (optional): ");
+            mName = sc.nextLine();
+            System.out.println("Enter last name: ");
+            lName = sc.nextLine();
+            System.out.println("Enter SSN: ");
+            ssn = sc.nextInt();
+
+                if (mName.equalsIgnoreCase("")) {
+                    mandatoryOptional user = new mandatoryOptional(fName, lName, ssn);
+                    System.out.println(user.fName + " " + user.mName + " " + user.lName + " " + user.ssn);
+                } else {
+                    mandatoryOptional user = new mandatoryOptional(fName, mName, lName, ssn); // complete your object; feel free to change its name!
+                    System.out.println(user.fName + " " + user.mName + " " + user.lName + " " + user.ssn);
+                }
+            userNumber++;
+
+            System.out.println("Enter 1 to enter another user: ");
+            again = sc.nextInt();
+            String decoy = sc.nextLine();
+        }while(again == 1);
 
 
     }
 }
-    //Second file/class: mandatoryOptional
-    //Not that the files are separated in the Project structure on the left
 class mandatoryOptional {
     // create two mandatory fields and one optional field minimum
-
+    public String fName;
+    public String mName;
+    public String lName;
+    public int ssn;
     // create Constructor(s) to handle input of mandatory and optional information
-
+    mandatoryOptional(String fName, String lName, int ssn){
+        this(fName, "N/A", lName, ssn);
+    }
+    mandatoryOptional(String fName, String mName, String lName, int ssn){
+        this.fName = fName;
+        this.mName = mName;
+        this.lName = lName;
+        this.ssn = ssn;
+    }
     // you may create getter and setter methods to take in and output user info
 
 }
